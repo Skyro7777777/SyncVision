3f74c5ff625f6eed7d0f869b67b7763c0daba21b
Skyro7777777 <skyro7777777@outlook.com>
2026-06-04 01:06:05 +0530

    Create CalculatorProfileProto.java

---
+++ app/src/main/java/com/google/mediapipe/proto/CalculatorProfileProto.java
@@ -0,0 +1,8 @@
+package com.google.mediapipe.proto;
+
+public final class CalculatorProfileProto {
+    private CalculatorProfileProto() {}
+    public static final class CalculatorProfile {
+        public CalculatorProfile() {}
+    }
+}
