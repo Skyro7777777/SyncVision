3d69bf13363aacdcb0c388aca2fc66b1c09016f9
Skyro7777777 <skyro7777777@outlook.com>
2026-06-04 01:06:49 +0530

    Add GraphTemplateProto class for graph templates

---
+++ app/src/main/java/com/google/mediapipe/proto/GraphTemplateProto.java
@@ -0,0 +1,8 @@
+package com.google.mediapipe.proto;
+
+public final class GraphTemplateProto {
+    private GraphTemplateProto() {}
+    public static final class CalculatorGraphTemplate {
+        public CalculatorGraphTemplate() {}
+    }
+}
